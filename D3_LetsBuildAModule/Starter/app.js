db4f0985435a33dda8f8ed01d890946bde9717d8
Sirfame Lin <sirfame@hotmail.com>
2018-03-20 23:08:52 -0700

    creating first module

---
+++ D3_LetsBuildAModule/Starter/app.js
@@ -0,0 +1,6 @@
+// Your Javascript Code Goes Here
+
+// Note: The 'starter' folders that appear in this course's downloadable source code is here to give you a starting point to try out coding yourself. The 'finished' folder contains the code as it is when we finish the lecture. You can use the 'finished' folder to compare to your own code, or just examine the finished code.
+// Require is a function that Node JS makes available in its JS core. First argument will be a string, is the location or the name of the module
+// ./ means same folder level
+require("./greet.js");
---
+++ D3_LetsBuildAModule/Starter/greet.js
@@ -0,0 +1,3 @@
+// First module, first set of JS code designed to be independent of other code, yet working in tandem with it, something I can reuse
+// and reference in other node programs, and is a way to keep code structured and easy to manage as it grows.
+console.log("Hello");
