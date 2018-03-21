4de7be2a2baf04c1e5cdc6ed2afe2a772d4f8ac7
Sirfame Lin <sirfame@hotmail.com>
2018-03-20 23:25:26 -0700

    built simple module

--- D3_LetsBuildAModule/Starter/app.js
+++ D3_LetsBuildAModule/Starter/app.js
@@ -2,5 +2,8 @@
 
 // Note: The 'starter' folders that appear in this course's downloadable source code is here to give you a starting point to try out coding yourself. The 'finished' folder contains the code as it is when we finish the lecture. You can use the 'finished' folder to compare to your own code, or just examine the finished code.
 // Require is a function that Node JS makes available in its JS core. First argument will be a string, is the location or the name of the module
-// ./ means same folder level
-require("./greet.js");
+// ./ means same folder level. no need to include .js, Node is smart enough to assume that the required module is JS
+var greet = require("./greet");
+
+// functions are first class, so greet is the function object, adding the parenthesis will call the function
+greet();
--- D3_LetsBuildAModule/Starter/greet.js
+++ D3_LetsBuildAModule/Starter/greet.js
@@ -1,3 +1,9 @@
 // First module, first set of JS code designed to be independent of other code, yet working in tandem with it, something I can reuse
 // and reference in other node programs, and is a way to keep code structured and easy to manage as it grows.
-console.log("Hello");
+// Code inside the module is protected, app.js cannot access the code unless specifically stated
+var greet = function() {
+    console.log("Hello module");
+};
+
+// When the module is called, the following line will give back whatever is included in the export
+module.exports = greet;
