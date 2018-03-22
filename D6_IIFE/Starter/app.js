20516ab6f5e234ac55f314712f5a5d46a0b6c550
Sirfame <sirfame@hotmail.com>
2018-03-22 16:51:15 -0700

    immediately invoked expressions

--- D6_IIFE/Starter/app.js
+++ D6_IIFE/Starter/app.js
@@ -1,1 +1,10 @@
 // Your Javascript Code Goes Here
+var firstname = "vs";
+
+(function (lastname) {
+    var firstname = "sf";
+    console.log(firstname);
+    console.log(lastname);
+})("lin");
+
+console.log(firstname);
