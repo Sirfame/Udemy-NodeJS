b961b3b9a37b1cd90ad171f0d201d045c2b6e05d
Sirfame <sirfame@hotmail.com>
2018-03-23 10:52:54 -0700

    module exports an object of name value pairs of other modules

---
+++ D9_MoreOnRequire/Starter/app.js
@@ -0,0 +1,5 @@
+// when there is no js file named in the require param, it will look for a folder.
+var greet = require('./greet');
+
+greet.english();
+greet.spanish();
---
+++ D9_MoreOnRequire/Starter/greet/index.js
@@ -0,0 +1,8 @@
+var english = require('./english');
+var spanish = require('./spanish');
+
+// name : var
+module.exports = {
+    english: english,
+    spanish: spanish
+}
