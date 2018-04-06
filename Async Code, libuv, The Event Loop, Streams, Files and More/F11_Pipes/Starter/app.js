4f746f5bf12878333c3ff2251454b14f4480be5f
Sirfame Lin <sirfame@hotmail.com>
2018-04-06 07:46:41 -0700

    understanding pipes

--- Async Code, libuv, The Event Loop, Streams, Files and More/F11_Pipes/Starter/app.js
+++ Async Code, libuv, The Event Loop, Streams, Files and More/F11_Pipes/Starter/app.js
@@ -1,10 +1,21 @@
 var fs = require('fs');
+var zlib = require('zlib');
 
-var readable = fs.createReadStream(__dirname + '/greet.txt', { encoding: 'utf8', highWaterMark: 16 * 1024 });
+
+var readable = fs.createReadStream(__dirname + '/greet.txt');
 
 var writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
 
-readable.on('data', function(chunk) {
-	console.log(chunk);
-	writable.write(chunk);
-});
+var compressed = fs.createWriteStream(__dirname + '/greetcopy.txt.gz');
+
+// simply giving a compressed output, gzip is both readable and writeable, so we can write to the writeable stream
+var gzip = zlib.createGzip();
+
+// sets up the event listener to listen for the chunk of data, then returns the writeable stream.
+// if the parameter was a duplex listener (read and write), we can call pipe on the destination again.
+readable.pipe(writable);
+
+// stream to stream to stream
+// this pattern is called method chaining.
+// gzip is not a file stream, it was a stream that did a particular job, in this case, compress
+readable.pipe(gzip).pipe(compressed);
