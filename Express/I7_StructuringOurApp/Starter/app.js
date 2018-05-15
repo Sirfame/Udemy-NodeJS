a500b904ad6e3ae960d3461bc2137634adb2b55f
Sirfame Lin <sirfame@hotmail.com>
2018-05-14 19:37:17 -0700

    using controllers

--- Express/I7_StructuringOurApp/Starter/app.js
+++ Express/I7_StructuringOurApp/Starter/app.js
@@ -1,11 +1,11 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+
 var app = express();
 
-var port = process.env.PORT || 3000;
+var apiController = require('./controllers/apiController');
+var htmlController = require('./controllers/htmlController');
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
-var jsonParser = bodyParser.json();
+var port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/public'));
 
@@ -16,31 +16,8 @@ app.use('/', function (req, res, next) {
 	next();
 });
 
-app.get('/', function(req, res) {
-	res.render('index');
-});
+htmlController(app);
+apiController(app);
 
-app.get('/person/:id', function(req, res) {
-	res.render('person', { ID: req.params.id, Qstr: req.query.qstr });
-});
-
-app.post('/person', urlencodedParser, function(req, res) {
-	res.send('Thank you!');
-	console.log(req.body.firstname);
-	console.log(req.body.lastname);
-});
-
-app.get('/api/person/:id', function(req, res) {
-	// get that data from database
-	res.json({ firstname: 'John', lastname: 'Doe' });
-});
-
-app.post('/api/person', jsonParser, function(req, res) {
-	// save to the database
-});
-
-app.delete('/api/person/:id', function(req, res) {
-	// delete from the database
-});
 
 app.listen(port);
