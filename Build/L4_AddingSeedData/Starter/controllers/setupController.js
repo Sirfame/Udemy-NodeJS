e3a85c8b2247d20e1a81556ebc8b5605b3f9a2b2
Sirfame <sirfame@hotmail.com>
2018-05-22 13:38:24 -0700

    seed data status

---
+++ Build/L4_AddingSeedData/Starter/controllers/setupController.js
@@ -0,0 +1,32 @@
+var Todos = require('../models/todoModel');
+
+module.exports = function(app) {
+    app.get('/api/setupTodos', function(req, res) {
+        // seed database
+        var starterTodos = [
+            {
+                username: 'test',
+                todo: 'Buy milk',
+                isDone = false,
+                hasAttachment: false
+            },
+            {
+                username: 'test',
+                todo: 'Feed dog',
+                isDone = false,
+                hasAttachment: false
+            },
+            {
+                username: 'test',
+                todo: 'Learn node',
+                isDone = false,
+                hasAttachment: false
+            }
+        ];
+
+        // the create function accepts an array, matches the schema, and takes an error first callback 
+        Todos.create(starterTodos, function(err, results) {
+            res.send(result);
+        });
+    });
+}
